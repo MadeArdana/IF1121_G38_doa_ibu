12d30154d46efea81bcac1bc7a898af885962b9d
Ghaniyul Amri Caulava <ghanivivo123@gmail.com>
2026-05-07 22:00:20 +0700

    Update fakta.pl

--- src/fakta.pl
+++ src/fakta.pl
@@ -1,87 +1,36 @@
-pria(loid).
-usia(loid, 135).
-wanita(yor).
-usia(yor, 132).
-menikah(loid, yor).
-pria(karma).
-usia(karma, 79).
-anak(karma, loid).
-anak(karma, yor).
-wanita(adinda).
-usia(adinda, 67).
-anak(adinda, loid).
-anak(adinda, yor).
-menikah(todoroki, adinda).
-pria(todoroki).
-usia(todoroki, 72).
-anak(wiliam, todoroki).
-anak(wiliam, adinda).
-pria(wiliam).
-usia(wiliam, 45).
-menikah(wiliam, kawakaze).
-wanita(kawakaze).
-usia(kawakaze, 37).
-anak(umaru, kawakaze).
-anak(umaru, wiliam).
-usia(umaru, 16).
-wanita(umaru).
-pria(levi).
-usia(levi, 69).
-anak(razi, levi).
-anak(razi, adinda).
-usia(razi, 42).
-pria(razi).
-menikah(razi, reze).
-wanita(reze).
-usia(reze, 35).
-anak(chopper, reze).
-anak(chopper, razi).
-usia(chopper, 8).
-pria(chopper).
-pria(faqih).
-usia(faqih, 109).
-wanita(karina).
-usia(karina, 97).
-menikah(faqih, karina).
-anak(owen, faqih).
-anak(owen, karina).
-usia(owen, 64).
-pria(owen).
-menikah(owen, tsunade).
-wanita(tsunade).
-usia(tsunade, 46).
-anak(fathur, tsunade).
-anak(fathur, owen).
-usia(fathur, 3).
-pria(fathur).
-anak(nezuko, tsunade).
-anak(nezuko, owen).
-usia(nezuko, 11).
-wanita(nezuko).
-anak(ariana, faqih).
-anak(ariana, karina).
-usia(ariana, 52).
-wanita(ariana).
-menikah(zoro, ariana).
-pria(zoro).
-usia(zoro, 57).
-anak(anya, zoro).
-anak(anya, ariana).
-umur(anya, 7).
-wanita(anya).
-saudara(X, Y) :- (anak(X, Z), anak(Y, Z), anak(X, A), anak(Y, A), menikah(Z, A), menikah(A, Z), X \= Y, A \= Z);       
-                 (anak(X, Z), anak(Y, Z), anak(X, A), anak(Y, B), menikah(Z, A), menikah(A, Z), menikah(Z, B), menikah(B, Z), X \= Y, A \= Z, A \= B, B \= Z).
-saudaratiri(X, Y) :- anak(X, Z), anak(Y, Z), anak(X, A), anak(Y, B), A \= B, Z \= A, Z \= B, X \= Y.
-sepupu(X, Y) :- anak(X, P1), anak(Y, P2), saudara_kandung(P1, P2), X \= Y.
-saudara_kandung(X, Y) :- anak(X, Ayah), anak(Y, Ayah), anak(X, Ibu), anak(Y, Ibu), Ayah \= Ibu, X \= Y.
-kakak(X, Y) :- (saudara_kandung(X, Y) ; saudaratiri(X, Y)), usia(X, UX), usia(Y, UY), UX > UY.
-keponakan(X, Y) :- anak(X, Z), (saudara_kandung(Z, Y) ; saudaratiri(Z, Y)).
-nenek(X, Y) :- wanita(X), anak(Y, P), anak(P, X).
-keturunan(X, Y) :- anak(X, Y). 
-keturunan(X, Y) :- anak(X, Z), keturunan(Z, Y).
-mertua(X, Y) :- menikah(Y, Z), anak(Z, X).
-lajang(X) :- \+ menikah(X, _), \+ menikah(_, X).
-anaksulung(X) :- anak(X, Y), \+ (anak(Z, Y), usia(Z, UZ), usia(X, UX), Z \= X, UZ > UX).
-anakbungsu(X) :- anak(X, Y), \+ (anak(Z, Y), usia(Z, UZ), usia(X, UX), Z \= X, UZ < UX).
-anaktunggal(X) :- anak(X, Y), \+ (anak(Z, Y), X \= Z).
-yatimpiatu(X) :- \+ anak(X, _).
+/* FAKTA STATIS */
+warna_biasa(merah).
+warna_biasa(kuning).
+warna_biasa(hijau).
+warna_biasa(biru).
+warna_spesial(hitam).
+
+jenis_angka(0).
+jenis_angka(1).
+jenis_angka(2).
+jenis_angka(3).
+jenis_angka(4).
+jenis_angka(5).
+jenis_angka(6).
+jenis_angka(7).
+jenis_angka(8).
+jenis_angka(9).
+
+jenis_aksi(skip).
+jenis_aksi(reverse).
+jenis_aksi(draw_two).
+
+jenis_wild(wild).
+jenis_wild(wild_draw_four).
+
+/* FAKTA DYNAMIC */
+:- dynamic(urutan_pemain/1).
+:- dynamic(giliran/1).
+:- dynamic(discard_top/1).
+:- dynamic(warna_aktif/1).
+:- dynamic(arah_permainan/1).
+:- dynamic(kartu_pemain/2).
+:- dynamic(permainan_aktif/0).
+:- dynamic(deck/1).
+:- dynamic(discard_pile/1).
+:- dynamic(temp_kartu/1).
